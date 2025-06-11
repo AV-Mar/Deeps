274263a3498e6645299fea2ad5285b0085d0575d
AV-Mar <misha.s.avm@gmail.com>
2025-06-12 02:19:10 +0500

    Read tokens from environment

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
-TELEGRAM_TOKEN = '7851905982:AAFE4yEGXZyW1WerAiyMaQPoJ6Pa7CZaRQc'
-OPENROUTER_API_KEY = 'sk-or-v1-e8944abefd37637da7e79aa32103626801df6fbf4d30b81fdd6db636deb3679'
+import os
+
+TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
+OPENROUTER_API_KEY = os.environ.get('OPENROUTER_API_KEY')
+
+if not TELEGRAM_TOKEN or not OPENROUTER_API_KEY:
+    raise EnvironmentError('Required environment variables TELEGRAM_TOKEN and OPENROUTER_API_KEY are not set')
